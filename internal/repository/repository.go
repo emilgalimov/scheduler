7de5e7ffb1ded61f59d15c6b7480068c1c14100c
emilgalimov <41144687+emilgalimov@users.noreply.github.com>
2022-05-15 15:11:13 +0300

    [feat] get all tasks

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -47,6 +47,10 @@ func (r *TasksRepository) GetTask(ctx context.Context, ID uint64) (task models.T
 	const sqlTS = `SELECT id, name, description, minutes_from_start, duration_minutes FROM task_stages WHERE task_id = $1`
 
 	rows, err := r.pool.Query(ctx, sqlTS, ID)
+	if err != nil {
+		return models.Task{}, err
+	}
+
 	for {
 		if !rows.Next() {
 			break
@@ -81,9 +85,59 @@ func (r *TasksRepository) CreateTaskStage(ctx context.Context, taskStage models.
 	return
 }
 
-func (TasksRepository) GetAllTasks(ctx context.Context) ([]models.Task, error) {
-	//TODO implement me
-	panic("implement me")
+func (r *TasksRepository) GetAllTasks(ctx context.Context) (tasks []models.Task, err error) {
+	//language=PostgreSQL
+	const sql = `SELECT 
+    t.id, 
+    t.name, 
+    t.description, 
+    ts.id, 
+    ts.name, 
+    ts.description, 
+    ts.minutes_from_start,
+    ts.duration_minutes
+    FROM tasks t LEFT JOIN task_stages ts on t.id = ts.task_id ORDER BY t.ID, ts.minutes_from_start`
+
+	row, err := r.pool.Query(ctx, sql)
+
+	if err != nil {
+		return nil, err
+	}
+
+	tasksMap := make(map[uint64]*models.Task)
+
+	for row.Next() {
+		values, _ := row.Values()
+
+		taskId := uint64(values[0].(int64))
+
+		if _, ok := tasksMap[taskId]; !ok {
+			tasksMap[taskId] = &models.Task{
+				ID:          taskId,
+				Name:        values[1].(string),
+				Description: values[2].(string),
+			}
+		}
+
+		if values[3] == nil {
+			continue
+		}
+
+		tasksMap[taskId].Stages = append(tasksMap[taskId].Stages,
+			models.TaskStage{
+				ID:               uint64(values[3].(int64)),
+				Name:             values[4].(string),
+				Description:      values[5].(string),
+				MinutesFromStart: time.Duration(values[6].(int64)) * time.Millisecond,
+				DurationMinutes:  time.Duration(values[7].(int64)) * time.Millisecond,
+			})
+	}
+
+	for _, v := range tasksMap {
+		tasks = append(tasks, *v)
+	}
+
+	return tasks, err
 }
 
 func (TasksRepository) CreateUserTask(ctx context.Context, userID uint64, taskID uint64) error {
