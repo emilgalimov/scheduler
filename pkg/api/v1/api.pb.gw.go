dfe3e7ae12c0c6e94a94c17f32f0ad9ad3308053
emilgalimov <41144687+emilgalimov@users.noreply.github.com>
2022-05-27 09:51:02 +0300

    [refactor] change protobuf body

--- pkg/api/v1/api.pb.gw.go
+++ pkg/api/v1/api.pb.gw.go
@@ -37,6 +37,14 @@ func request_SmartCalendar_CreateUser_0(ctx context.Context, marshaler runtime.M
 	var protoReq CreateUserRequest
 	var metadata runtime.ServerMetadata
 
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
 	msg, err := client.CreateUser(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
@@ -45,6 +53,14 @@ func request_SmartCalendar_CreateUser_0(ctx context.Context, marshaler runtime.M
 func local_request_SmartCalendar_CreateUser_0(ctx context.Context, marshaler runtime.Marshaler, server SmartCalendarServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq CreateUserRequest
 	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := server.CreateUser(ctx, &protoReq)
 	return msg, metadata, err
